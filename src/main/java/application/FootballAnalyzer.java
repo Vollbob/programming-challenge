e2417ced6017b70d9ebdab1096de0f9eaeb42edd
Vollbob <132945599+Vollbob@users.noreply.github.com>
2025-08-04 13:59:29 +0200

    add FootballAnalyzer and tests for it

---
+++ src/main/java/application/FootballAnalyzer.java
@@ -0,0 +1,31 @@
+package application;
+
+import java.util.List;
+
+import model.FootballTeamData;
+
+public class FootballAnalyzer implements DataAnalyzer<FootballTeamData>{
+
+	@Override
+	public String findMinimumDifference(List<FootballTeamData> footballData) {
+		int minimumDifference;
+    	String teamName = "";
+    	
+    	if (footballData.size() == 0) {
+    		return teamName;
+    	} else {
+    		minimumDifference = Math.abs(footballData.get(0).getGoals() - footballData.get(0).getGoals_allowed());
+    		teamName = footballData.get(0).getTeam();
+    	}
+    	
+    	for (int i = 1; i < footballData.size(); i++) {
+    		int actualDifference = Math.abs(footballData.get(i).getGoals() - footballData.get(i).getGoals_allowed());
+    		if (actualDifference < minimumDifference) {
+    			minimumDifference = actualDifference;
+    			teamName = footballData.get(0).getTeam();
+    		}
+    	}
+        return teamName;
+	}
+
+}
---
+++ src/test/java/application/FootballAnalyzerTest.java
@@ -0,0 +1,72 @@
+package application;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import model.FootballTeamData;
+
+public class FootballAnalyzerTest {
+	
+	@Test
+    void correctSmallestSpread() throws IOException, URISyntaxException{
+		List<FootballTeamData> footballData = new ArrayList<FootballTeamData>();
+		
+		FootballTeamData footballData1 = new FootballTeamData();
+		footballData1.setTeam("Arsenal");
+		footballData1.setGoals(79);;
+		footballData1.setGoals_allowed(36);
+		
+		FootballTeamData footballData2 = new FootballTeamData();
+		footballData2.setTeam("Liverpool");
+		footballData2.setGoals(67);;
+		footballData2.setGoals_allowed(30);
+		
+		FootballTeamData footballData3 = new FootballTeamData();
+		footballData3.setTeam("Manchester");
+		footballData3.setGoals(87);
+		footballData3.setGoals_allowed(45);
+		
+		footballData.add(footballData1);
+		footballData.add(footballData2);
+		footballData.add(footballData3);
+		
+		FootballAnalyzer analyzer = new FootballAnalyzer();
+		
+    	assertEquals("Arsenal", analyzer.findMinimumDifference(footballData), "The team with the smallest distance should be Arsenal.");
+    }
+	
+	@Test
+    void correctSmallestSpreadEqual() throws IOException, URISyntaxException{
+List<FootballTeamData> footballData = new ArrayList<FootballTeamData>();
+		
+		FootballTeamData footballData1 = new FootballTeamData();
+		footballData1.setTeam("Liverpool");
+		footballData1.setGoals(79);;
+		footballData1.setGoals_allowed(36);
+		
+		FootballTeamData footballData2 = new FootballTeamData();
+		footballData2.setTeam("Arsenal");
+		footballData2.setGoals(79);;
+		footballData2.setGoals_allowed(36);
+		
+		FootballTeamData footballData3 = new FootballTeamData();
+		footballData3.setTeam("Manchester");
+		footballData3.setGoals(79);
+		footballData3.setGoals_allowed(36);
+		
+		footballData.add(footballData1);
+		footballData.add(footballData2);
+		footballData.add(footballData3);
+		
+		FootballAnalyzer analyzer = new FootballAnalyzer();
+		
+    	assertEquals("Liverpool", analyzer.findMinimumDifference(footballData), "The team with the smallest distance should be Liverpool.");
+    }
+
+}
